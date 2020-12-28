123074cbc072e8bd03f47d9e45e3d50bbee5edb3
Alex Fung <alexfungshunhang@gmail.com>
2020-12-28 17:07:28 +0800

    Updated exit key binding

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ books = []  #this is used to store new inputs
 while(True):
 	user_selection = input("Enter '1' to add a new book | Enter '2' to view all books | Enter '3' to quit the program\n")
 
+	if(user_selection == str(0)):    #str(0) == '0'
+		break
+
 	if(user_selection == str(1)):    #str(1) == '1'
 		book_name = input("Enter Book Name\n")
 		books.append(book_name)
@@ -11,5 +14,3 @@ while(True):
 		for book in books:   #books is a list, each iteration handles an element as book
 			print(book)
 
-	elif(user_selection == str(3)):
-		break	#end of program (break while loop)
